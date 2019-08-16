5a6a024fe2fec94cb9afd88bc1511563ef4d6487
Gabriel Rascovit <rascovit@gmail.com>
2019-08-16 18:44:59 -0300

    add select2 to the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,13 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require select2.min
+//= require i18n/pt-BR
 //= require_tree .
+
+$(document).ready(function () {
+  $('select').select2({
+    theme: 'bootstrap4',
+    language: 'pt-BR'
+  });
+});
