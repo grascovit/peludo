4a6f1cbb075fb1dc5416071f45b1cd41af75216a
Gabriel Rascovit <rascovit@gmail.com>
2019-09-04 23:37:37 -0300

    add devise token auth

---
+++ db/migrate/20190905013739_add_devise_token_auth_columns.rb
@@ -0,0 +1,15 @@
+class AddDeviseTokenAuthColumns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :provider, :string, null: false, default: 'email'
+    add_column :users, :uid, :string, null: false, default: ''
+    add_column :users, :tokens, :json
+
+    User.find_each do |user|
+      user.uid = user.email
+      user.provider = 'email'
+      user.save!
+    end
+
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
