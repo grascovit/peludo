d6d27f9fcca7ce16c36c5f55fded84f0a1843435
Gabriel Rascovit <rascovit@gmail.com>
2019-11-22 01:07:16 -0300

    dont ask for current password if user came from oauth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+  EMAIL_PROVIDER = 'email'
+
   def destroy
     resource.deactivate!
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message! :notice, :destroyed
     yield resource if block_given?
     respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    return super if resource.provider == EMAIL_PROVIDER
+
+    resource.update_without_password(params)
   end
 end
