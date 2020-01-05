ff3ff31a5fd965b7b91840726c92fa0f825d7922
Gabriel Rascovit <rascovit@gmail.com>
2020-01-05 01:08:01 -0300

    fix error when user already signed up using google

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,15 @@
 class RegistrationsController < Devise::RegistrationsController
   EMAIL_PROVIDER = 'email'
 
+  def create
+    if user_already_exists?
+      set_flash_message! :alert, :already_signed_up_with_google
+      redirect_to(new_user_session_path) && return
+    end
+
+    super
+  end
+
   def destroy
     resource.deactivate!
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
@@ -17,5 +26,11 @@ class RegistrationsController < Devise::RegistrationsController
     return super if resource.provider == EMAIL_PROVIDER
 
     resource.update_without_password(params)
+  end
+
+  private
+
+  def user_already_exists?
+    User.exists?(email: sign_up_params['email'])
   end
 end
